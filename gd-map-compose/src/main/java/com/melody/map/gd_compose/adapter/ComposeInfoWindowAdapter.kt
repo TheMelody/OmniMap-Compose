0cfb4069e880f7be8f5993c55b443034a8181324
被风吹过的夏天 <developer_melody@163.com>
2022-10-27 17:35:18 +0800

    使用com.melody.map.gd_compose.overlay.Marker组合项的时候，给InfoWindowAdapter增加一个默认的InfoContent来弹信息窗

--- gd-map-compose/src/main/java/com/melody/map/gd_compose/adapter/ComposeInfoWindowAdapter.kt
+++ gd-map-compose/src/main/java/com/melody/map/gd_compose/adapter/ComposeInfoWindowAdapter.kt
@@ -22,10 +22,14 @@
 
 package com.melody.map.gd_compose.adapter
 
+import android.content.Context
 import android.graphics.Color
+import android.view.Gravity
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.Space
+import android.widget.TextView
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionContext
 import androidx.compose.ui.platform.ComposeView
@@ -52,10 +56,14 @@ internal class ComposeInfoWindowAdapter(
 
     override fun getInfoContents(marker: Marker): View? {
         // https://lbs.amap.com/api/android-sdk/guide/draw-on-map/draw-marker
-        // 高德地图的问题：这里必须返回一个空的View，否则getInfoContents回调的Marker是一个SDK默认的：空值Marker
+        // 当实现此方法并返回有效值时（返回值不为空，则视为有效）,SDK 将不会使用默认的样式，而采用此方法返回的样式
         val markerNode = markerNodeFinder(marker) ?: return Space(mapView.context)
         val infoContent  = markerNode.infoContent
         if (infoContent == null) { // 这里返回null，是处理getInfoWindow这2个方法谁返回视图的并显示冲突的问题
+            if(markerNode.infoWindow == null){
+                // 由于上面返回了一个SpaceView，这里返回一个，啥都没有设置的默认视图内容
+                return getDefaultInfoContent(marker, mapView.context)
+            }
             return null
         }
         return infoWindowView.applyAndRemove(false,markerNode.compositionContext) {
@@ -65,7 +73,7 @@ internal class ComposeInfoWindowAdapter(
 
     override fun getInfoWindow(marker: Marker): View? {
         // https://lbs.amap.com/api/android-sdk/guide/draw-on-map/draw-marker
-        // 高德地图的问题：这里必须返回一个空的View，否则getInfoWindow回调的Marker是一个SDK默认的：空值Marker
+        // 当实现此方法并返回有效值时（返回值不为空，则视为有效）,SDK 将不会使用默认的样式，而采用此方法返回的样式
         val markerNode = markerNodeFinder(marker) ?: return Space(mapView.context)
         val infoWindow  = markerNode.infoWindow
         if (infoWindow == null) { // 这里返回null，是处理getInfoContents这2个方法谁返回视图的并显示冲突的问题
@@ -73,6 +81,29 @@ internal class ComposeInfoWindowAdapter(
         }
         return infoWindowView.applyAndRemove(true,markerNode.compositionContext) {
             infoWindow(marker)
+        }
+    }
+
+    /**
+     * 这算是高德地图给开发者的bug，不太灵活，增加这个方法是为了给[com.melody.map.gd_compose.overlay.Marker]这个组合项使用的
+     * 返回一个默认的InfoContent视图，保证[com.melody.map.gd_compose.overlay.Marker]能正常弹出信息窗
+     */
+    private fun getDefaultInfoContent(marker: Marker,context: Context): View {
+        return LinearLayout(context).apply {
+            layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT)
+            orientation = LinearLayout.VERTICAL
+            addView(getDefaultInfoTextView(context,marker.title))
+            if(!marker.snippet.isNullOrBlank()){
+                addView(getDefaultInfoTextView(context,marker.snippet))
+            }
+        }
+    }
+    private fun getDefaultInfoTextView(context: Context,content: String) : TextView {
+        return TextView(context).apply {
+            gravity = Gravity.CENTER_HORIZONTAL
+            isSingleLine = true
+            setTextColor(Color.BLACK)
+            text = content
         }
     }
 
