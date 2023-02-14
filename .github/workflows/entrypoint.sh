adf5055b5d090fa45052bbcffe33224e63dc3042
被风吹过的夏天 <developer_melody@163.com>
2023-02-14 16:38:02 +0800

    Update entrypoint.sh

--- .github/workflows/entrypoint.sh
+++ .github/workflows/entrypoint.sh
@@ -2,4 +2,4 @@
 
 hub checkout ${${REPO_BRANCH}:-master}
 VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle`
-hub release create -a ./${APP_FOLDER}/build/outputs/apk/debug/*-debug.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
+hub release create -a ./${APP_FOLDER}/build/outputs/apk/release/*-release.apk -m "${RELEASE_TITLE} - v${VERSION_NAME}" $(date +%Y%m%d%H%M%S)
