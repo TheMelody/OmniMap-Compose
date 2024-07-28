f4adb207515be26f13ba34a8ea387438077a68f5
TheMelody <developer_melody@163.com>
2024-07-28 17:53:10 +0800

    update build.gradle.kts

--- sample-baidu/build.gradle.kts
+++ sample-baidu/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
